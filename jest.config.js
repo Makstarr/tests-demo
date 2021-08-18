f73ba7c9680d3ad079800a206bb7b083b2f5621b
Максим Тарасов <maks@Maksims-MacBook-Pro.local>
2021-08-18 12:20:53 +0300

    add coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   globals: {
     "ts-jest": {
       tsconfig: "tsconfig.jest.json",
